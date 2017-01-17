71b5f890ad53d47ab4f99556fe7195efee655867
sho <sho_ws@outlook.com>
2017-01-17 17:28:39 +0900

    行きたい

--- app_main.rb
+++ app_main.rb
@@ -33,7 +33,7 @@ post '/callback' do
 				elsif event.message['text'] =~ /行きたい/
 					client.reply_message(event['replyToken'], reply_botton_schedule)
 				elsif event.message['text'] =~ /呼んでみただけ/
-					client.reply_message(even['replyToken'], reply_message("もう (おこ)"))
+					client.reply_message(even['replyToken'], reply_message('もう'))
 
 				elsif event.message['text'] =~ /寝かせて/
           #client.reply_message(event['replyToken'], reply_message('少しお待ちください'))
