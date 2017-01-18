3c158db148fa37122af88ae20c224c8a9e185944
sho <sho_ws@outlook.com>
2017-01-18 16:54:00 +0900

    db

---
+++ db/migrate/20170117102904_create_keeps.rb
@@ -0,0 +1,9 @@
+class CreateKeeps < ActiveRecord::Migration[5.0]
+  def change
+  	create_table :keeps do |t|
+  		t.string :channel
+  		t.string :json
+  		t.timestamps null: false
+  	end
+  end
+end
