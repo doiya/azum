3fbfac3b0b7b023551c419ec81b8bb1aa8648454
sho <sho_ws@outlook.com>
2017-01-18 19:24:59 +0900

    fix

--- app_main.rb
+++ app_main.rb
@@ -56,7 +56,7 @@ post '/callback' do
 		when Line::Bot::Event::Postback
 			if event["postback"]["data"] =~ /行きたい/
 				#client.reply_message(event['replyToken'], reply_botton_schedule)
-				#client.reply_message(event['replyToken'], reply_message("おっけー\nおすすめのイベントを紹介するね"))
+				client.reply_message(event['replyToken'], reply_message("おっけー\nおすすめのイベントを紹介するね"))
 				client.reply_message(event['replyToken'], reply_carousel_museums(reply_museum_datas))
 
 			elsif event["postback"]['data'] =~ /呼んだだけ/
