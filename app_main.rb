823ff36247657a653114a3c7ce65df96bbcdedb1
sho <sho_ws@outlook.com>
2017-01-18 18:40:48 +0900

    fix

--- app_main.rb
+++ app_main.rb
@@ -34,8 +34,8 @@ post '/callback' do
 					client.reply_message(event['replyToken'], reply_confirm_start)
 
 				elsif event.message['text'] =~ /寝かせて/
-          #client.reply_message(event['replyToken'], reply_message('少しお待ちください'))
-          client.reply_message(event['replyToken'], reply_carousel_museums(reply_museum_datas))
+          client.reply_message(event['replyToken'], reply_message(reply_museum_datas.to_s))
+          #client.reply_message(event['replyToken'], reply_carousel_museums(reply_museum_datas))
         elsif event.message['text'] =~ /情報/
 	        client.reply_message(event['replyToken'], reply_template_museum(reply_museum_data))
 
